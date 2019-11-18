3b272deccf0d441beb671f64602809db4042a31d
Benjamin Leray <b.leray35@gmail.com>
2019-11-18 17:33:45 +0100

    display navbar footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -60,34 +60,3 @@ mother_d.user = user
 mother_d.save
 
 puts " Seed Done !!"
-
-  # new_table "mothers", force: :cascade do |t|
-  #   t.string "name"
-  #   t.integer "age"
-  #   t.string "location"
-  #   t.integer "price"
-  #   t.text "description"
-  #   t.string "hobbies"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.bigint "user_id"
-  #   t.index ["user_id"], name: "index_mothers_on_user_id"
-  # end
-
-
-
-
-
-
-
-  # create_table "users", force: :cascade do |t|
-  #   t.string "email", default: "", null: false
-  #   t.string "encrypted_password", default: "", null: false
-  #   t.string "reset_password_token"
-  #   t.datetime "reset_password_sent_at"
-  #   t.datetime "remember_created_at"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.string "name"
-  #   t.index ["email"], name: "index_users_on_email", unique: true
-  #   t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
