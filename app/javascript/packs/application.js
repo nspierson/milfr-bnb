568a8193769bf0b2661da43b56c1cd42ffc0dabf
t.c <thomascaen@hotmail.fr>
2019-11-20 13:07:00 +0100

    la modal checkout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,18 @@
 import "bootstrap";
-import "../plugins/flatpickr"
+import "../plugins/flatpickr";
+import modal from "../plugins/modal";
+
+modal();
+
+const element = document.querySelector('#closeButton')
+
+if (element) {
+  element.addEventListener('click',(event) => {
+const fin = document.querySelector("#booking-button")
+fin.defaultValue = "Booked!"
+})
+
+}
+
 
 
---
+++ app/javascript/plugins/modal.js
@@ -0,0 +1,11 @@
+const modal = () => {
+  const element = document.querySelector('#booking-button')
+  element.addEventListener('click',(event) => {
+    const date = document.querySelector('#range_start').value.split(' to ');
+    document.querySelector('#modalStart').innerHTML = date[0];
+    document.querySelector('#modalEnd').innerHTML = date[1];
+    const prix = document.querySelector('#price-assessment');
+    document.querySelector('#modalPrix').innerHTML = prix.innerHTML;
+  })
+}
+export default modal;
